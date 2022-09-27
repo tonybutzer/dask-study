8822fc38d8f29d28d6c2d822632cc4b9cfef4a3e
EC2 Default User <ec2-user@ip-10-12-65-99.us-west-2.compute.internal>
2022-09-27 23:16:11 +0000

    git make publish - tony - 9/2022

--- conda/build_pangeo.sh
+++
@@ -1,9 +0,0 @@
-# sudo chown 1000 -R /opt/miniconda
-conda config --add channels conda-forge --force
-source activate base
-conda install -y mamba
-#conda create -y -n gabe mamba
-#conda activate gabe
-mamba env create -f pangeo_env.yml
-
-
